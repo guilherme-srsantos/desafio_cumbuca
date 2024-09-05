0e1596f0c23fb4d1c40313c2c975243067770464
= <=>
2024-09-05 11:10:25 -0300

    feat: initial commit

---
+++ lib/desafio_cumbuca.ex
@@ -0,0 +1,19 @@
+defmodule DesafioCumbuca do
+  @moduledoc """
+  Documentation for `DesafioCumbuca`.
+  """
+
+  @doc """
+  ## Examples
+
+      iex> DesafioCumbuca.main(1)
+
+  """
+
+  def main(args) do
+    IO.puts("It works!")
+    "It works!"
+  end
+
+
+end
---
+++ test/desafio_cumbuca_test.exs
@@ -0,0 +1,8 @@
+defmodule DesafioCumbucaTest do
+  use ExUnit.Case
+  doctest DesafioCumbuca
+
+  test "greets the world" do
+    assert DesafioCumbuca.main(1) == "It works!"
+  end
+end
